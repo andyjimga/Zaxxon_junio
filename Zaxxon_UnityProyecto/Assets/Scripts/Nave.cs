b76a8de502d1efb141b5f8010d65943e6b54996a
andyjimga <andyjimga@gmail.com>
2021-04-26 18:50:52 +0200

    Movimiento nave
    faltan aun las restricciones y la velocidad

--- Zaxxon_UnityProyecto/Assets/Scripts/Nave.cs
+++ Zaxxon_UnityProyecto/Assets/Scripts/Nave.cs
@@ -6,8 +6,9 @@ public class Nave : MonoBehaviour
 {
 
     private Vector3 posIni;
-
-
+    [SerializeField] Vector3 posDest = new Vector3(0, 1, 0);
+    private Vector3 finPos;
+    
 
     void Start()
     {
@@ -17,6 +18,29 @@ public class Nave : MonoBehaviour
 
     void Update()
     {
-        
+        //MovNaveProvisional();
+        MovNave();
+    }
+
+    /* void MovNaveProvisional()
+     {
+
+         float desplY = Input.GetAxis("Vertical");
+
+         finPos = posIni + posDest * desplY * Time.deltaTime;
+         print(finPos);
+
+         posIni = finPos;
+         transform.position = posIni;
+     }*/
+
+    void MovNave()
+    {
+        float desplY = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.up * desplY * Time.deltaTime); //añadir speed
+
+        float desplX = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * desplX * Time.deltaTime); //añadir speed
+
     }
 }
